bdc2209373a917b06cc82e26a80419b4db518f49
john <hadassafrank@gmail.com>
2022-03-03 20:43:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
+import axios from "axios";
+
+import Header from "./components/Header";
+import Home from "./containers/Home";
+import Signup from "./containers/Signup";
+import Login from "./containers/Login";
+import Publish from "./containers/Publish";
+import Offer from "./containers/Offer";
+import Payment from "./containers/Payment";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch, faCheck, faRedo } from "@fortawesome/free-solid-svg-icons";
+library.add(faSearch, faCheck, faRedo);
+
+function App() {
+  const [token, setToken] = useState(Cookies.get("token") || null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [sortPrice, setSortPrice] = useState(false);
+  const [fetchRangeValues, setFetchRangeValues] = useState([0, 10000]);
+  const [search, setSearch] = useState("");
+
+  const setUser = (token) => {
+    if (token) {
+      setToken(token);
+      Cookies.set("token", token);
+    } else {
+      setToken(null);
+      Cookies.remove("token");
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://lereacteur-vinted-api.herokuapp.com/offers?priceMin=${
+          fetchRangeValues[0]
+        }&priceMax=${fetchRangeValues[1]}&sort=${
+          sortPrice ? "price-desc" : "price-asc"
+        }&title=${search}`
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [fetchRangeValues, sortPrice, search]);
+  return (
+    <Router>
+      <Header
+        setUser={setUser}
+        token={token}
+        setFetchRangeValues={setFetchRangeValues}
+        fetchRangeValues={fetchRangeValues}
+        sortPrice={sortPrice}
+        setSortPrice={setSortPrice}
+        setSearch={setSearch}
+      />
+      <Routes>
+        <Route path="/" element={<Home data={data} isLoading={isLoading} />} />
+        <Route path="/signup" element={<Signup setUser={setUser} />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/publish" element={<Publish token={token} />} />
+        <Route path="/offer/:id" element={<Offer />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
